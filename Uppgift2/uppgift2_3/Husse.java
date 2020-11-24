49690d303454ce233ff514606b89a338b411dcc3
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-11-24 22:15:04 +0100

    Create Husse.java

---
+++ Uppgift2/uppgift2_3/Husse.java
@@ -0,0 +1,13 @@
+package uppgift2_3;
+
+public class Husse {
+	String name = "Matti";
+	
+	Dog Bonzo = new Dog("Bonzo", "Terverense");
+	Dog Stina = new Dog("Stina", "Labrador");
+	Dog Nova = new Dog("Nova", "German shepherd");
+	
+	public String toString() {
+		return "Jag heter " + name + " och har tre hundar som heter " + Bonzo.n + ", " + Stina.n + " och " + Nova.n + ".";
+	}
+}
