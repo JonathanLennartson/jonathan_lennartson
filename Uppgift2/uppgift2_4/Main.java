fa23ab9ba3ff0cc60ff45e5337bd7880c4716eb1
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-11-24 22:32:34 +0100

    Update Main.java

--- Uppgift2/uppgift2_4/Main.java
+++ Uppgift2/uppgift2_4/Main.java
@@ -4,12 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Matte matte = new Matte();
-		Cat cat = new Cat();	
-		
+
 		System.out.println(matte.toString());
-		
+
 		System.out.println("Hey cat, whos your owner?");
-		System.out.println(cat.whosOwner());
+		System.out.println(Cat.whosOwner());
 	}
 
 }
