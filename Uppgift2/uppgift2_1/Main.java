ae83181539d024bece04f4bfa4a71b1523c1a8af
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-11-24 10:23:10 +0100

    Create Main.java

---
+++ Uppgift2/uppgift2_1/Main.java
@@ -0,0 +1,14 @@
+package uppgift2_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		
+		System.out.println("The dogs race is: " + dog.race + ",\nand it's name is: " + dog.name + ".\n");
+		System.out.println("The cats race is: " + cat.race + ",\nand it's name is: " + cat.name + ".");
+
+	}
+
+}
