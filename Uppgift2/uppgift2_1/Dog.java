bb24acfd2a4a993dcc6f357aae2471d57b91d456
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-11-25 11:23:46 +0100

    Update Dog.java

--- Uppgift2/uppgift2_1/Dog.java
+++ Uppgift2/uppgift2_1/Dog.java
@@ -3,14 +3,10 @@ package uppgift2_1;
 public class Dog extends Animal {
 	String race;
 	String name;
-	
-		Dog (String type, String race, String name) {
-			super (type);
-			this.race = race;
-			this.name = name;
-		}
-		
-		public void getName(String name) {
-			this.name = name;
-		}
+
+	Dog(String type, String race, String name) {
+		super(type);
+		this.race = race;
+		this.name = name;
+	}
 }
