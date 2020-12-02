b0f11c6eb7438175e8e83001525acf80a61172d3
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-02 13:02:53 +0100

    Update StopWatch.java
    Lade till att sista utskrivna siffran sparades i variabel stopTime.

--- Uppgift3/Uppgift3/StopWatch.java
+++ Uppgift3/Uppgift3/StopWatch.java
@@ -4,26 +4,26 @@ public class StopWatch extends Thread {
 	private volatile boolean counting = true;
 	private int minute = 0;
 	private int seconds = 0;
-	private int miliSeconds = 0;
+	private int milliSeconds = 0;
+	String stopTime;
 
 	public void run() {
 		while (counting) {
-			System.out.println(minute + ":" + seconds + "," + miliSeconds);
-			miliSeconds++;
-			if (miliSeconds == 1000) {
-				miliSeconds = 000;
+			System.out.println(minute + ":" + seconds + "," + milliSeconds);
+			milliSeconds++;
+			if (milliSeconds == 1000) {
+				milliSeconds = 000;
 				seconds++;
 			} else if (seconds == 60) {
 				seconds = 0;
 				minute++;
-
 			}
 			try {
 				Thread.sleep(1);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			stopTime = stopTime.valueOf(minute + ":" + seconds + "," + milliSeconds);
 		}
 	}
 
