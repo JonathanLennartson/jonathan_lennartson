706b5fe55e44a4f091d1ef84f9920afa2c619003
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-06 14:52:07 +0100

    Update Cat.java

--- Uppgift2/uppgift2_1/Cat.java
+++ Uppgift2/uppgift2_1/Cat.java
@@ -1,12 +1,21 @@
 package uppgift2_1;
 
-public class Cat extends Animal {
-	String race;
-	String name;
-
-	Cat(String type, String race, String name) {
-		super(type);
+public class Cat {
+	private String race;
+	private String name;
+	
+	public String getRace() {
+		return race;
+	}
+	public void setRace(String race) {
 		this.race = race;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
 		this.name = name;
 	}
+
+	
 }
