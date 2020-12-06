03a9b686c05c042a48414bb957d5f6061ce19be7
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-06 14:52:45 +0100

    Update Husse.java

--- Uppgift2/uppgift2_3/Husse.java
+++ Uppgift2/uppgift2_3/Husse.java
@@ -1,13 +1,37 @@
 package uppgift2_3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Husse {
-	String name = "Matti";
-	
-	Dog Bonzo = new Dog("Bonzo", "Terverense");
-	Dog Stina = new Dog("Stina", "Labrador");
-	Dog Nova = new Dog("Nova", "German shepherd");
-	
-	public String toString() {
-		return "Jag heter " + name + " och har tre hundar som heter " + Bonzo.name + ", " + Stina.name + " och " + Nova.name + ".";
+	Scanner scanner = new Scanner(System.in);
+	ArrayList<Dog> list = new ArrayList<Dog>();
+
+	String name;
+	int dogs;
+
+	public void name() {
+		System.out.println("Vad heter matte?");
+		this.name = scanner.nextLine();
 	}
+
+	public void howManyDogs() {
+		System.out.println("Hur många hundar " + name + "?");
+		this.dogs = scanner.nextInt();
+	}
+
+	public void dogList() {
+		System.out.println("Vad heter hundarna?");
+		Dog dog = new Dog();
+		for (int i = 0; i < dogs; i++) {
+			dog.setName(scanner.nextLine());
+			list.add(i, dog);
+		}
+
+	}
+
+	public String toString() {
+		return "Jag heter " + name + " och har " + dogs + " hundar som heter " + list;
+	}
+
 }
