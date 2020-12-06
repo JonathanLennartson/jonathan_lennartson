7339fc0699b331792bf409fc41db2ee4c98dc4c2
JonathanLennartson <jonathan.lennartson@gmail.com>
2020-12-06 18:24:13 +0100

    Update Main.java

--- Uppgift2/uppgift2_3/Main.java
+++ Uppgift2/uppgift2_3/Main.java
@@ -1,11 +1,25 @@
 package uppgift2_3;
 
+import uppgift2_1.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Husse Matti = new Husse();
+		Husse husse = new Husse();
+		Dog dog = new Dog();
 		
-		System.out.println(Matti.toString());
-	}
-
+		husse.setName("Matti");
+		husse.setHowManyDogs(4);
+		
+		String[] dogList = new String[husse.getHowManyDogs()];	
+		
+		dogList[0] = dog.setName("Bonzo");
+		dogList[1] = dog.setName("Kalle");
+		dogList[2] = dog.setName("Gaya");
+		dogList[3] = dog.setName("Faya");
+		
+		System.out.println("Jag heter " + husse.getName() + " och har " + husse.getHowManyDogs() + " hundar som heter " + dogList[0] + ", " + dogList[1] +", "+ dogList[2] + " och " + dogList[3] + ".");
+		
+		
+	} 
 }
